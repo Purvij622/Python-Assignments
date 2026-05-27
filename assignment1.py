0890b32ae6c268619c08dc9b9c1be371d013fdc7
Purvij622 <purvijain583@gmail.com>
2026-05-27 22:43:38 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+# --------------------------------
+# Practice Print Statement
+# --------------------------------
+
+print("Hello", "Python", "World")
+
+print("Akshat", "Rahul", "Neha", sep="-")
+
+print("Welcome", end=" ")
+print("Students")
+
+
+# --------------------------------
+# Input from User
+# --------------------------------
+
+name = input("Enter your name : ")
+
+print("Hello", name, "Welcome to Python")
